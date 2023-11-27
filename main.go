e1a476deb01378136f549d01cfbb0aec1a3a38af
Saranpat Prasertthum <Saranpatp@gmail.com>
2023-11-27 14:42:36 -0600

    + fix save in wrong folder cuz of go routine

--- main.go
+++ main.go
@@ -1,23 +1,23 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"io"
-	"log"
-	"net/http"
-	"os"
-	"path/filepath"
-	"strings"
-	"sync"
-	"time"
+    "bufio"
+    "fmt"
+    "io"
+    "log"
+    "net/http"
+    "os"
+    "path/filepath"
+    "strings"
+    "sync"
+    "time"
 
-	"github.com/joho/godotenv"
+    "github.com/joho/godotenv"
 )
 
 var (
-    client *http.Client
-    wg     sync.WaitGroup
+    client    *http.Client
+    wg        sync.WaitGroup
     semaphore chan struct{}
 )
 
@@ -53,7 +53,6 @@ func downloadFile(url, filepath string) error {
     return err
 }
 
-
 func main() {
     // Load .env file
     err := godotenv.Load()
@@ -67,12 +66,11 @@ func main() {
 
     baseurl := "https://download.gatedata.org/"
     bizTypes := []string{"spot", "futures_usdt"}
-	// dataTypes := []string{"deals", "orderbooks", "candlesticks", "trades", "mark_prices", "funding_updates", "funding_applies"}
 
-	bizTypeToDataTypes := map[string][]string{
-		"spot":         {"deals", "orderbooks", "candlesticks"},
-		"futures_usdt": {"trades", "orderbooks", "mark_prices", "funding_updates", "funding_applies"},
-	}
+    bizTypeToDataTypes := map[string][]string{
+        "spot":         {"deals", "orderbooks", "candlesticks"},
+        "futures_usdt": {"trades", "orderbooks", "mark_prices", "funding_updates", "funding_applies"},
+    }
 
     start, _ := time.Parse("2006-01-02", startDateStr)
     end, _ := time.Parse("2006-01-02", endDateStr)
@@ -88,53 +86,55 @@ func main() {
             if err := os.MkdirAll(bizDir, os.ModePerm); err != nil {
                 log.Fatalf("Error creating biz directory %s: %v", bizDir, err)
             }
-			// doing type check here
-			dataTypes, ok := bizTypeToDataTypes[biz]
-			
-			if !ok {
-				log.Printf("No data types defined for bizType: %s", biz)
-				continue
-			}
-            for _, dataType := range dataTypes {
-				for d := start; d.Before(end); d = d.AddDate(0, 0, 1) { // Looping through days
-					year := d.Year()
-					month := fmt.Sprintf("%02d", d.Month())
-					day := fmt.Sprintf("%02d", d.Day())
-			
-					if dataType == "orderbooks" {
-						// Handle hourly logs for order books
-						for hour := 0; hour < 24; hour++ {
-                            wg.Add(1)
 
+            dataTypes, ok := bizTypeToDataTypes[biz]
+            if !ok {
+                log.Printf("No data types defined for bizType: %s", biz)
+                continue
+            }
+
+            for _, dataType := range dataTypes {
+                dataTypeDir := filepath.Join(bizDir, dataType)
+                if err := os.MkdirAll(dataTypeDir, os.ModePerm); err != nil {
+                    log.Fatalf("Error creating data type directory %s: %v", dataTypeDir, err)
+                }
+
+                for d := start; d.Before(end); d = d.AddDate(0, 0, 1) {
+                    year := d.Year()
+                    month := fmt.Sprintf("%02d", d.Month())
+                    day := fmt.Sprintf("%02d", d.Day())
+
+                    if dataType == "orderbooks" {
+                        for hour := 0; hour < 24; hour++ {
+                            wg.Add(1)
                             semaphore <- struct{}{} // Acquire a slot in the semaphore
 
-                            go func(hour int) {
-							hourStr := fmt.Sprintf("%02d", hour)
-							url := fmt.Sprintf("%s%s/%s/%d%s/%s-%d%s%s%s.csv.gz", baseurl, biz, dataType, year, month, market, year, month, day, hourStr)
-							filepath := filepath.Join(bizDir, fmt.Sprintf("%s-%s-%d%s%s%s.csv.gz", market, dataType, year, month, day, hourStr))
+                            go func(hour int, dataType string) {
+                                hourStr := fmt.Sprintf("%02d", hour)
+                                url := fmt.Sprintf("%s%s/%s/%d%s/%s-%d%s%s%s.csv.gz", baseurl, biz, dataType, year, month, market, year, month, day, hourStr)
+                                filepath := filepath.Join(dataTypeDir, fmt.Sprintf("%s-%s-%d%s%s%s.csv.gz", market, dataType, year, month, day, hourStr))
                                 
-							fmt.Println("Downloading", url)
+                                fmt.Println("Downloading", url)
                                 if err := downloadFile(url, filepath); err != nil {
                                     fmt.Println("Error downloading file:", err)
                                 }
-                            }(hour)
-						}
-					} else {
+                            }(hour, dataType)
+                        }
+                    } else {
                         wg.Add(1)
                         semaphore <- struct{}{} // Acquire a slot in the semaphore
-                        go func() {
-						// Handle other data types
-						url := fmt.Sprintf("%s%s/%s/%d%s/%s-%d%s.csv.gz", baseurl, biz, dataType, year, month, market, year, month)
-						filepath := filepath.Join(bizDir, fmt.Sprintf("%s-%s-%d%s.csv.gz", market, dataType, year, month))
-			
-						fmt.Println("Downloading", url)
-						if err := downloadFile(url, filepath); err != nil {
-							fmt.Println("Error downloading file:", err)
-						}
-                        }()
-					}
-				}
-			}
+                        go func(dataType string) {
+                            url := fmt.Sprintf("%s%s/%s/%d%s/%s-%d%s.csv.gz", baseurl, biz, dataType, year, month, market, year, month)
+                            filepath := filepath.Join(dataTypeDir, fmt.Sprintf("%s-%s-%d%s.csv.gz", market, dataType, year, month))
+                            
+                            fmt.Println("Downloading", url)
+                            if err := downloadFile(url, filepath); err != nil {
+                                fmt.Println("Error downloading file:", err)
+                            }
+                        }(dataType)
+                    }
+                }
+            }
         }
     }
     wg.Wait()
